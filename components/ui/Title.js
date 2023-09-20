6fbf9a01bc9b4131218532d70f1a32c0a9bd37a4
Junaidahmed111 <ahmedjuniad30@gmail.com>
2023-09-20 23:56:13 +0500

    setting up dynamic widths

--- components/ui/Title.js
+++ components/ui/Title.js
@@ -6,13 +6,15 @@ function Title({ children }) {
 export default Title;
 const styles = StyleSheet.create({
   title: {
-    fontFamily:'open-sans-bold',
+    fontFamily: "open-sans-bold",
     fontSize: 24,
     fontWeight: "bold",
     color: "#ddb52f",
     textAlign: "center",
     borderWidth: 2,
-    borderColor: "#ddb52f",  
+    borderColor: "#ddb52f",
     padding: 12,
+    maxWidth: "80%",
+    width: 300,
   },
 });
--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -106,6 +106,7 @@ const styles = StyleSheet.create({
   screen: {
     flex: 1,
     padding: 24,
+    alignItems: 'center',
   },
   instText: {
     marginBottom: 12,
